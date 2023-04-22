a6b3d94c6cf858c643eb2c5f1be23c3c4b1895a1
Korede <olakorede.salami@gmail.com>
2023-04-22 13:22:45 +0100

    logo resizing

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import { Route, Routes, Link } from 'react-router-dom';
 import { MdClose } from "react-icons/md"
 import { FiMenu } from "react-icons/fi"
-import logo from '../images/Logo3.webp'
+import logo from '../images/Logo5.png'
 
 function Header() {
     const [navbarOpen, setNavbarOpen] = useState(false)
